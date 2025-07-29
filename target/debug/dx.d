ef0431ed0f21fc7de567809d233e7aa798146ef4
ajju40959 <ajju40959@gmail.com>
2025-07-29 15:16:09 +0000

    feat: dx main

--- target/debug/dx.d
+++ target/debug/dx.d
@@ -1,1 +1,1 @@
-/home/user/dx/target/debug/dx: /home/user/dx/src/header/default.dxcf /home/user/dx/src/header/mod.rs /home/user/dx/src/main.rs /home/user/dx/src/platform/mod.rs /home/user/dx/src/platform/unix.rs
+/home/user/dx/target/debug/dx: /home/user/dx/src/main.rs
